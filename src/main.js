f7b98fe19b91e6b07b816f45d711f60597515792
Jaron Armiger <jaron.armiger@gmail.com>
2023-02-11 16:58:36 -0800

    add lenis

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
+import Lenis from '@studio-freight/lenis'
 
-createBadge()
-animateTitle()
+console.log('it works')
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: 'vertical', // vertical, horizontal
+  gestureDirection: 'vertical', // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+
+//get scroll value
+lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+  console.log({ scroll, limit, velocity, direction, progress })
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
